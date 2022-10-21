e82d1158b5ae86d670b9307e2c51da20848d3df1
Nana-shi <leof.pereira2@gmail.com>
2022-10-21 09:10:43 -0300

    Atividades da semana 6 do projeto Intro a Web

--- script.js
+++ script.js
@@ -66,7 +66,7 @@ if (anime1.dublagem === true){
 if(anime2.dublagem === true){
     animes.push(anime2)
 } else{
-    alert("O anime \"Steins;Gate\" não tem dublagem e não será adicionado!")
+    // alert("O anime \"Steins;Gate\" não tem dublagem e não será adicionado!")
 }
 
 if(anime3.dublagem === true){
@@ -132,7 +132,7 @@ const filtroItem = (array, string) =>{
             if(objeto[item] == string){
                 console.log(objeto[item].toString())
             } else{
-                alert("Nenhum item foi encontrado nesse objeto!!")
+                // alert("Nenhum item foi encontrado nesse objeto!!")
             }
             // if(objeto[item] === string){
             //     //console.log(item)
@@ -143,3 +143,67 @@ const filtroItem = (array, string) =>{
     } }
 
 filtroItem(animes, 'Full Metal Alchemist Brotherhood')
+
+//Atividades Semana 6
+
+const container = document.getElementById("container")
+container.innerHTML += "<div id='busca'>"
+const busca = document.getElementById("busca")
+busca.innerHTML += "<input type='text' id='nome'>"
+busca.innerHTML += "<button type='submit' onclick='filtrarItem(event)'>Buscar</button>"
+
+container.innerHTML += "<section class='bloco' id='anime1'></section>"
+anime1 = document.getElementById("anime1")
+anime1.innerHTML += "<img class='imagem1' src='https://img1.ak.crunchyroll.com/i/spire4/8056a82e973dde98ebb82abd39dc69731564519729_full.jpg' width='200px'>"
+anime1.innerHTML += "<ul class='lista' id='lista1'>"
+const lista1 = document.getElementById("lista1")
+lista1.innerHTML += "<li>Nome: <a href='https://myanimelist.net/anime/21/One_Piece'>One Piece</a></li>"
+lista1.innerHTML += "<li>Numero de Episodios: 1032</li>"
+lista1.innerHTML += "<li>Tem dublagem oficial (PT/BR): Sim</li>"
+lista1.innerHTML += "<li>Alguns personagens: Luffy, Zoro, Sanji</li>"
+
+container.innerHTML += "<section class='bloco' id='anime2'></section>"
+anime2 = document.getElementById("anime2")
+anime2.innerHTML += "<img class='imagem2' src='https://img1.ak.crunchyroll.com/i/spire2/ff06631478e5344b467111c7aa85ff881302201468_full.jpg' width='200px'>"
+anime2.innerHTML += "<ul class='lista' id='lista2'>"
+const lista2 = document.getElementById("lista2")
+lista2.innerHTML += "<li>Nome: <a href='https://myanimelist.net/anime/9253/Steins_Gate'>Steins;Gate</a></li>"
+lista2.innerHTML += "<li>Numero de Episodios: 24</li>"
+lista2.innerHTML += "<li>Tem dublagem oficial (PT/BR): Não</li>"
+lista2.innerHTML += "<li>Alguns personagens: Okabe, Kurisu, Mayuri</li>"
+
+container.innerHTML += "<section class='bloco' id='anime3'></section>"
+anime3 = document.getElementById("anime3")
+anime3.innerHTML += "<img class='imagem3' src='https://img1.ak.crunchyroll.com/i/spire4/fabddf1040abbd18948b9aacc18011b31475523493_full.jpg' width='200px'>"
+anime3.innerHTML += "<ul class='lista' id='lista3'>"
+const lista3 = document.getElementById("lista3")
+lista3.innerHTML += "<li>Nome: <a href='https://myanimelist.net/anime/5114/Fullmetal_Alchemist__Brotherhood'>Full Metal Alchemist: Brotherhood</a></li>"
+lista3.innerHTML += "<li>Numero de Episodios: 64</li>"
+lista3.innerHTML += "<li>Tem dublagem oficial (PT/BR): Sim</li>"
+lista3.innerHTML += "<li>Alguns personagens: Edward, Alphonse, Mustang</li>"
+
+function filtrarItem(event){
+    const nome = document.getElementById("nome")      
+    if(nome.value === "One Piece"){
+        const anime1 = document.getElementById("anime1")
+        const anime2 = document.getElementById("anime2")
+        const anime3 = document.getElementById("anime3")
+        anime1.setAttribute('class', 'bloco')
+        anime2.setAttribute('class', 'escondido')
+        anime3.setAttribute('class', 'escondido')
+    } else if(nome.value === "Steins;Gate"){
+        const anime1 = document.getElementById("anime1")
+        const anime2 = document.getElementById("anime2")
+        const anime3 = document.getElementById("anime3")
+        anime1.setAttribute('class', 'escondido')
+        anime2.setAttribute('class', 'bloco')
+        anime3.setAttribute('class', 'escondido')
+    }   else if(nome.value === "Full Metal Alchemist"){
+        const anime1 = document.getElementById("anime1")
+        const anime2 = document.getElementById("anime2")
+        const anime3 = document.getElementById("anime3")
+        anime1.setAttribute('class', 'escondido')
+        anime2.setAttribute('class', 'escondido')
+        anime3.setAttribute('class', 'bloco')
+    }       
+}
